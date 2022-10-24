4f3ee82afadf018251ccbef3ef3bd955250c665f
siam50 <siamurrahman50@gmail.com>
2022-10-24 23:19:47 +0600

    navbar Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import edu1 from '../../assets/education1.jpg';
+import edu2 from '../../assets/education2.jpg'
+import logo from '../../assets/skill.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            <h3>Home Page</h3>
+            <Container className='mt-5'>
+                <Row>
+                    <Col lg='6'>
+                        <Carousel>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src={edu1}
+                                    alt="First slide"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Reaserch Mathodology</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src={edu2}
+                                    alt="Second slide"
+                                />
+
+                                <Carousel.Caption>
+                                    <h3>Graphic Design</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                    </Col>
+                    <Col lg='6'>
+                        <Card>
+                            <Card.Header className='text-decoration-none text-warning' as="h5">
+                                <img
+                                    alt=""
+                                    src={logo}
+                                    width="30"
+                                    height="30"
+                                    className="d-inline-block align-top"
+                                />
+                                Grow Your Skill
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Title >Learn a new skill online on your time</Card.Title>
+                                <Card.Text className='text-start'>
+                                    “The most profound words will remain unread unless you can keep the learner engaged. You can't see their eyes to know if they got it so … say it, show it, write it, demo it and link it to an activity.” — James Bates.
+                                </Card.Text>
+                                <Card.Text className='text-start'>
+                                    People often tout interactivity as the great benefit offered by eLearning, yet most interactivity does nothing to either engage or instruct.
+                                </Card.Text>
+                                <Card.Text className='text-start'>
+                                    One of the most important areas we can develop as professionals is competence in accessing and sharing knowledge.Online learning is not the next big thing, it is the now big thing.eLearning is changing.And we will see new models new technologies and designs emerge. So, lets drop the or at least give it a new and wider definition.
+                                </Card.Text>
+                                <Link to='/courses'><Button variant="primary">Go For Courses</Button></Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/skill.png';
 
 const Header = () => {
     return (
         <div>
-            <h3>This Is Header Part</h3>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand>
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        <Link to='/' className='text-decoration-none text-warning'>Grow Your Skill</Link>
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Link className='me-lg-3 text-decoration-none text-light' to='/courses'>Courses</Link>
+                            <Link className='me-lg-3 text-decoration-none text-light' to='/courses'>FAQ</Link>
+                            <Link className='me-lg-3 text-decoration-none text-light' to='/blog'>Blog</Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
