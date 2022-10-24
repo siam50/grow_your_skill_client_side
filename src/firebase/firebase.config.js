15db66fa2523720f033f97ba35e0bcf985f224a0
siam50 <siamurrahman50@gmail.com>
2022-10-24 21:45:29 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCP5oeJuRxfZ5vz-et5IfB-TvsxJUTZL24",
+    authDomain: "grow-your-skill-a9858.firebaseapp.com",
+    projectId: "grow-your-skill-a9858",
+    storageBucket: "grow-your-skill-a9858.appspot.com",
+    messagingSenderId: "603518602905",
+    appId: "1:603518602905:web:ab7f88d419160a6a77b208"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
